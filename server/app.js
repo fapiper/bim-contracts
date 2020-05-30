e31f9cb7b09300a67a022b218e73983c22c02590
Fabian Piper <fabianpiper@web.de>
2020-05-30 13:54:52 +0200

    Execute client + server scripts in parallel

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 const loggingMiddleware = (req, res, next) => {
   console.log('Request at', req.originalUrl);

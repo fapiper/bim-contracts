e45fabfc0041715dae62fb9598d2137eedfee4fb
Fabian Piper <fabianpiper@web.de>
2020-07-28 12:39:18 +0200

    Init quasar framework as client bundle

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,68 +1,10 @@
 <template>
-  <Layout>
-    <v-sheet color="grey lighten-4" class="px-3 pt-3 pb-3">
-      <v-skeleton-loader
-        class="mx-auto"
-        max-width="400"
-        type="card"
-      ></v-skeleton-loader>
-      <v-skeleton-loader
-        class="mx-auto"
-        max-width="400"
-        type="card"
-      ></v-skeleton-loader>
-      <v-skeleton-loader
-        class="mx-auto"
-        max-width="400"
-        type="card"
-      ></v-skeleton-loader>
-      <v-skeleton-loader
-        class="mx-auto"
-        max-width="400"
-        type="card"
-      ></v-skeleton-loader>
-      <v-skeleton-loader
-        class="mx-auto"
-        max-width="400"
-        type="card"
-      ></v-skeleton-loader>
-    </v-sheet>
-  </Layout>
+  <div id="q-app">
+    <router-view />
+  </div>
 </template>
-
 <script>
-import Layout from './layouts/default.vue';
 export default {
-  props: {
-    source: String,
-  },
-  components: {
-    Layout,
-  },
-  data: () => ({
-    drawer: null,
-    items: [
-      { icon: 'lightbulb_outline', text: 'Notes' },
-      { icon: 'touch_app', text: 'Reminders' },
-      { divider: true },
-      { heading: 'Labels' },
-      { icon: 'add', text: 'Create new label' },
-      { divider: true },
-      { icon: 'archive', text: 'Archive' },
-      { icon: 'delete', text: 'Trash' },
-      { divider: true },
-      { icon: 'settings', text: 'Settings' },
-      { icon: 'chat_bubble', text: 'Trash' },
-      { icon: 'help', text: 'Help' },
-      { icon: 'phonelink', text: 'App downloads' },
-      { icon: 'keyboard', text: 'Keyboard shortcuts' },
-    ],
-  }),
-};
-</script>
-
-<style>
-#keep .v-navigation-drawer__border {
-  display: none;
+  name: 'App'
 }
-</style>
+</script>
--- server/app.js
+++ server/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 
 const { handleError } = require('./helpers/error');
 const routes = require('./routes');
 
 const app = express();
-
 const errorMiddleware = (err, req, res, next) => handleError(err, res);
 
-app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
 
+app.use(morgan('dev'));
 app.use('/', errorMiddleware);
 app.use('/', routes);
 

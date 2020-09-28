a078d4d9a60e9fb6e4c2c5be7223db568fe466ef
Fabian Piper <fabianpiper@web.de>
2020-09-28 13:00:00 +0200

    Cleanup project model

--- src/models/project-model.js
+++ src/models/project-model.js
@@ -1,62 +1,38 @@
 import Web3 from 'web3';
-import BoQ from 'src/models/boq-model.js';
-import Billing from 'src/models/billing-model.js';
+import { Utils } from 'quasar';
 
 class Project {
-  constructor(
-    name,
-    designation,
-    description,
-    building_contractor,
-    general_contractor,
-    sub_contractors,
-    billing,
-    boqs
-  ) {
+  constructor(_id, name, actors, created) {
+    this._id = _id;
     this.name = name;
-    this.designation = designation;
-    this.description = description;
-    this.building_contractor = building_contractor;
-    this.general_contractor = general_contractor;
-    this.sub_contractors = sub_contractors;
-    this.created = new Date().toJSON();
-    this.hash = Web3.utils.sha3(this.name + this.created);
-    this.billing = billing;
-    this.boqs = boqs;
+    this.actors = actors;
+    this.created = created;
+  }
+
+  get hash() {
+    return Web3.utils.sha3(JSON.stringify(this));
   }
 
   static toStore(project) {
     return {
-      hash: project.hash,
+      _id: project._id,
       name: project.name,
-      designation: project.designation,
-      description: project.description,
-      building_contractor: project.building_contractor,
-      general_contractor: project.general_contractor,
-      sub_contractors: project.sub_contractors,
+      actors: project.actors,
       created: project.created,
-      billing: Billing.toStore(project.billing),
-      boqs: project.boqs.map(BoQ.toStore),
     };
   }
 
-  static fromView(project, billing, boqs, address) {
+  static fromStore(project) {
     return new Project(
       project.name,
-      project.designation,
-      project.description,
-      {
-        name: 'Muster Bauherr',
-        address: 0x0,
-      },
-      {
-        name: 'Muster Generalunternehmer',
-        address: address,
-      },
-      [],
-      billing,
-      boqs
+      project._id,
+      project.actors,
+      project.created
     );
+  }
+
+  static build(name, actors) {
+    return new Project(Utils.uid(), name, actors, new Date().toJSON());
   }
 }
 

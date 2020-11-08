3df6f6f7acb5a4736e282a748dabb74170c46cc4
Fabian Piper <fabianpiper@web.de>
2020-11-08 18:02:39 +0100

    Agreement loader

--- client/src/pages/Project/Assignments.vue
+++ client/src/pages/Project/Assignments.vue
@@ -1,53 +1,59 @@
 <template>
   <q-page padding>
-    <div class="row items-start q-gutter-md">
-      <ServiceContractCard
-        v-for="assignment in assignments"
-        class="full-width"
-        :key="assignment.hash"
-        :contract="assignment"
-        type="assignment"
-        @assign="showDialog"
-      />
-    </div>
-    <q-dialog v-model="prompt">
-      <q-card style="min-width: 450px">
-        <q-card-section class="row items-center">
-          <div class="text-h6">Auftragsvergabe</div>
-          <q-space />
-          <q-btn icon="close" flat round dense v-close-popup />
-        </q-card-section>
+    <template v-if="loading">
+      <div class="row justify-center">
+        <q-spinner color="grey-6" size="3em" />
+      </div>
+    </template>
+    <template v-else>
+      <div class="row items-start q-gutter-md">
+        <ServiceContractCard
+          v-for="assignment in assignments"
+          class="full-width"
+          :key="assignment.hash"
+          :contract="assignment"
+          type="assignment"
+          @assign="showDialog"
+        />
+      </div>
+      <q-dialog v-model="prompt">
+        <q-card style="min-width: 450px">
+          <q-card-section class="row items-center">
+            <div class="text-h6">Auftragsvergabe</div>
+            <q-space />
+            <q-btn icon="close" flat round dense v-close-popup />
+          </q-card-section>
 
-        <q-card-section class="q-pt-none">
-          <p>
-            Geben Sie zur Auftragsvergabe
-            <span class="text-weight-bold">{{
-              selected && (selected.short_desc || selected.name)
-            }}</span>
-            bitte den gewünschten Auftragnehmer an:
-          </p>
-          <q-input
-            filled
-            dense
-            placeholder="Adresse"
-            hint="Blockchain Identität"
-            v-model="address"
-            autofocus
-            @keyup.enter="prompt = false"
-          />
-        </q-card-section>
+          <q-card-section class="q-pt-none">
+            <p>
+              Geben Sie zur Auftragsvergabe
+              <span class="text-weight-bold">{{
+                selected && (selected.short_desc || selected.name)
+              }}</span>
+              bitte den gewünschten Auftragnehmer an:
+            </p>
+            <q-input
+              filled
+              dense
+              placeholder="Adresse"
+              hint="Blockchain Identität"
+              v-model="address"
+              autofocus
+              @keyup.enter="prompt = false"
+            />
+          </q-card-section>
 
-        <q-card-actions align="center">
-          <q-btn
-            unelevated
-            class="full-width"
-            color="primary"
-            label="Auftrag vergeben"
-            @click="assign"
-          />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
+          <q-card-actions align="center">
+            <q-btn
+              unelevated
+              class="full-width"
+              color="primary"
+              label="Auftrag vergeben"
+              @click="assign"
+            />
+          </q-card-actions>
+        </q-card> </q-dialog
+    ></template>
   </q-page>
 </template>
 
@@ -59,6 +65,7 @@ export default {
   components: { ServiceContractCard },
   data() {
     return {
+      loading: true,
       assignments: [],
       prompt: false,
       selected: null,
@@ -70,13 +77,17 @@ export default {
   },
   methods: {
     async loadAssignments() {
-      this.loading = true;
-      const projectId = this.$route.params.project;
-      this.assignments = await this.$db.agreement.getAssignmentsByProject(
-        projectId,
-        this.$auth.user().address
-      );
-      this.loading = false;
+      try {
+        this.loading = true;
+        const projectId = this.$route.params.project;
+        this.assignments = await this.$db.agreement.getAssignmentsByProject(
+          projectId,
+          this.$auth.user().address
+        );
+        this.loading = false;
+      } catch (error) {
+        console.error(error);
+      }
     },
     showDialog(service) {
       this.selected = service;
--- client/src/pages/Project/Awards.vue
+++ client/src/pages/Project/Awards.vue
@@ -1,30 +1,36 @@
 <template>
   <q-page padding>
-    <div class="row items-start q-gutter-md">
-      <ServiceContractCard
-        v-for="award in awards"
-        class="full-width"
-        :key="award.hash"
-        :contract="award"
-        type="award"
-        @payAgreement="payAgreement"
-      />
-    </div>
-    <q-dialog v-model="payAlert">
-      <q-card>
-        <q-card-section>
-          <div class="text-h6">Leistungsvertrag abgeschlossen</div>
-        </q-card-section>
+    <template v-if="loading">
+      <div class="row justify-center">
+        <q-spinner color="grey-6" size="3em" />
+      </div>
+    </template>
+    <template v-else>
+      <div class="row items-start q-gutter-md">
+        <ServiceContractCard
+          v-for="award in awards"
+          class="full-width"
+          :key="award.hash"
+          :contract="award"
+          type="award"
+          @payAgreement="payAgreement"
+        />
+      </div>
+      <q-dialog v-model="payAlert">
+        <q-card>
+          <q-card-section>
+            <div class="text-h6">Leistungsvertrag abgeschlossen</div>
+          </q-card-section>
 
-        <q-card-section class="q-pt-none">
-          Eine Zahlung in Höhe von {{ price }} € wurde initiiert.
-        </q-card-section>
+          <q-card-section class="q-pt-none">
+            Eine Zahlung in Höhe von {{ price }} € wurde initiiert.
+          </q-card-section>
 
-        <q-card-actions align="right">
-          <q-btn flat label="Okay" color="primary" v-close-popup />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
+          <q-card-actions align="right">
+            <q-btn flat label="Okay" color="primary" v-close-popup />
+          </q-card-actions>
+        </q-card> </q-dialog
+    ></template>
   </q-page>
 </template>
 
@@ -35,7 +41,7 @@ export default {
   name: 'PageProjectAwards',
   components: { ServiceContractCard },
   data() {
-    return { awards: [], payAlert: false, price: 0 };
+    return { loading: true, awards: [], payAlert: false, price: 0 };
   },
   mounted() {
     this.loadAwards();
@@ -44,25 +50,26 @@ export default {
     async payAgreement(agreement) {
       let totalPrice = 0;
       agreement.services.forEach((service) => {
-        totalPrice += service.billing_item ? service.billing_item.price : 0;
+        totalPrice += service.billing_item ? service.billing_item.price : 0.0;
       });
-      console.log('pay', agreement, totalPrice);
       this.$q.loading.show();
       await this.$db.agreement.pay(agreement);
-
       this.price = totalPrice;
+      agreement.payed = true;
       this.$q.loading.hide();
       this.payAlert = true;
-      agreement.payed = true;
     },
     async loadAwards() {
-      this.loading = true;
-      this.awards = await this.$db.agreement.getAwardsByProject(
-        this.$route.params.project,
-        this.$auth.user().address
-      );
-      console.log('this.awards', this.awards);
-      this.loading = false;
+      try {
+        this.loading = true;
+        this.awards = await this.$db.agreement.getAwardsByProject(
+          this.$route.params.project,
+          this.$auth.user().address
+        );
+        this.loading = false;
+      } catch (error) {
+        console.error(error);
+      }
     },
   },
 };
